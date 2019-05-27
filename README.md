2163d0f557e51914c3827d1f3ac1e358a3b92b1c
George Fraser <george@fivetran.com>
2019-05-26 21:34:10 -0700

    Move color functions to their own file

--- README.md
+++ README.md
@@ -26,7 +26,9 @@ This extension gives VSCode support for [tree-sitter](http://tree-sitter.github.
 
 ### Fixing colorization of an existing language
 
-If you see something getting colored wrong, or something that should be colored but isn't, please make a PR to fix it! Colorization is performed by the various `colorLanguage(x, editor)` functions at the top of [extension.ts](https://github.com/georgewfraser/vscode-tree-sitter/blob/master/src/extension.ts). When working on the colorization rules, please keep in mind two core principles:
+If you see something getting colored wrong, or something that should be colored but isn't, you can help! The simplest way to help is to create an issue with a simple example, a screenshot, and an explanation of what is wrong. 
+
+You are also welcome to fix the problem yourself and submit a PR. Colorization is performed by the various `colorLanguage(x, editor)` functions in `src/colors.ts`. When working on the colorization rules, please keep in mind two core principles:
 
 1. Good colorization is *consistent*. It's better to not color at all than to color inconsistently.
 2. Good colorization is *selective*. The fewer things that we color, the more emphasis the color gives.
@@ -36,8 +38,9 @@ If you see something getting colored wrong, or something that should be colored 
 It's straightforward to add any [language with a tree-sitter grammar](https://tree-sitter.github.io/tree-sitter/).
 
 1. Add a dependency on the npm package for that language: `npm install tree-sitter-yourlang`.
-3. Add the language and color function to the [dictionary in ./lib/extension.ts](https://github.com/georgewfraser/vscode-tree-sitter/blob/758d5227dee704570125ea86a0a5ff2e0371a1e9/src/extension.ts#L172).
-4. Add a **simplified** TextMate grammar to `./textmate/yourlang.tmLanguage.json`. The job of this textmate grammar is just to color keywords and literals.
+2. Add a color function to `./lib/colors.ts`
+3. Add a language to the dictionary at the top of `./lib/extension.ts`
+4. Add a **simplified** TextMate grammar to `./textmate/yourlang.tmLanguage.json`. The job of this textmate grammar is just to color keywords and simple literals; anything tricky should be left white and colored by your color function.
 5. Add a reference to the grammar to the [contributes.grammars section of package.json](https://github.com/georgewfraser/vscode-tree-sitter/blob/fb4400b78481845c6a8497d079508d28aea25c19/package.json#L26). `yourlang` must be a [VSCode language identifier](https://code.visualstudio.com/docs/languages/identifiers).
 6. Add a reference to `onLanguage:yourlang` to the [activationEvents section of package.json](https://github.com/georgewfraser/vscode-tree-sitter/blob/fb4400b78481845c6a8497d079508d28aea25c19/package.json#L18). `yourlang` must be a [VSCode language identifier](https://code.visualstudio.com/docs/languages/identifiers).
 7. Add an example to `examples/yourlang`.
--- src/extension.ts
+++ src/extension.ts
@@ -1,314 +1,17 @@
 import * as vscode from 'vscode'
 import * as Parser from 'web-tree-sitter'
 import * as path from 'path'
-import * as colors from './scopes'
+import * as scopes from './scopes'
+import * as colors from './colors'
 
-type ColorFunction = (x: Parser.SyntaxNode, editor: vscode.TextEditor) => [Parser.SyntaxNode, string][]
-
-function colorGo(root: Parser.SyntaxNode, editor: vscode.TextEditor) {
-	// Guess package names based on paths
-	var packages: {[id: string]: boolean} = {}
-	function scanImport(x: Parser.SyntaxNode) {
-		if (x.type == 'import_spec') {
-			const str = x.firstChild!.text
-			const full = str.substring(1, str.length - 1)
-			const parts = full.split('/')
-			const last = parts[parts.length - 1]
-			packages[last] = true
-		}
-		for (const child of x.children) {
-			scanImport(child)
-		}
-	}
-	// Keep track of local vars that shadow packages
-	const allScopes: Scope[] = []
-	class Scope {
-		private locals = new Map<string, {modified: boolean, references: Parser.SyntaxNode[]}>()
-		private parent: Scope|null
-	
-		constructor(parent: Scope|null) {
-			this.parent = parent
-			allScopes.push(this)
-		}
-
-		declareLocal(id: string) {
-			if (this.locals.has(id)) {
-				this.locals.get(id)!.modified = true
-			} else {
-				this.locals.set(id, {modified: false, references: []})
-			}
-		}
-
-		modifyLocal(id: string) {
-			if (this.locals.has(id)) this.locals.get(id)!.modified = true
-			else if (this.parent != null) this.parent.modifyLocal(id)
-		}
-
-		referenceLocal(x: Parser.SyntaxNode) {
-			const id = x.text
-			if (this.locals.has(id)) this.locals.get(id)!.references.push(x)
-			else if (this.parent != null) this.parent.referenceLocal(x)
-		}
-	
-		isLocal(id: string): boolean {
-			if (this.locals.has(id)) return true
-			if (this.parent != null) return this.parent.isLocal(id)
-			return false
-		}
-
-		isModified(id: string): boolean {
-			if (this.locals.has(id)) return this.locals.get(id)!.modified
-			if (this.parent != null) return this.parent.isModified(id)
-			return false
-		}
-
-		modifiedLocals(): Parser.SyntaxNode[] {
-			const all = []
-			for (const {modified, references} of this.locals.values()) {
-				if (modified) {
-					all.push(...references)
-				}
-			}
-			return all
-		}
-
-		isPackage(id: string): boolean {
-			return packages[id] && !this.isLocal(id)
-		}
-
-		isRoot(): boolean {
-			return this.parent == null
-		}
-	}
-	const colors: [Parser.SyntaxNode, string][] = []
-	function scan(x: Parser.SyntaxNode, scope: Scope) {
-		const visible = isVisible(x, editor)
-		switch (x.type) {
-			case 'import_declaration':
-				scanImport(x)
-				break
-			case 'parameter_declaration':
-			case 'var_spec':
-			case 'const_spec':
-				if (!scope.isRoot()) {
-					for (const id of x.children) {
-						if (id.type == 'identifier') {
-							scope.declareLocal(id.text)
-						}
-					}
-				}
-				scanChildren(x, scope)
-				break
-			case 'short_var_declaration': 
-				if (!scope.isRoot()) {
-					for (const id of x.firstChild!.children) {
-						if (id.type == 'identifier') {
-							scope.declareLocal(id.text)
-						}
-					}
-				}
-				scanChildren(x, scope)
-				break
-			case 'inc_statement':
-			case 'dec_statement':
-				if (!scope.isRoot()) {
-					scope.modifyLocal(x.firstChild!.text)
-				}
-				scanChildren(x, scope)
-				break
-			case 'assignment_statement':
-				if (!scope.isRoot()) {
-					for (const id of x.firstChild!.children) {
-						if (id.type == 'identifier') {
-							scope.modifyLocal(id.text)
-						}
-					}
-				}
-				scanChildren(x, scope)
-				break
-			case 'identifier':
-				if (!scope.isRoot()) {
-					scope.referenceLocal(x)
-				}
-				if (visible && x.parent!.type == 'function_declaration') {
-					// func f() { ... }
-					colors.push([x, 'entity.name.function'])
-				}
-				break
-			case 'function_declaration':
-			case 'method_declaration':
-			case 'func_literal':
-			case 'block':
-				// Skip top-level declarations that aren't visible
-				if (visible || !scope.isRoot()) {
-					scanChildren(x, new Scope(scope))
-				}
-				break
-			case 'type_identifier':
-				if (visible) {
-					// x: type
-					colors.push([x, 'entity.name.type'])
-				}
-				break
-			case 'field_identifier':
-				if (visible) {
-					// pkg.member
-					const isPackage = x.parent!.type == 'selector_expression' && scope.isPackage(x.parent!.firstChild!.text)
-					// obj.member
-					if (!isPackage) colors.push([x, 'variable'])
-				}
-				break
-			default:
-				// Skip top-level declarations that aren't visible
-				if (visible || !scope.isRoot()) {
-					scanChildren(x, scope)
-				}
-		}
-	}
-	function scanChildren(x: Parser.SyntaxNode, scope: Scope) {
-		for (const child of x.children) {
-			scan(child, scope)
-		}
-	}
-	scan(root, new Scope(null))
-	for (const scope of allScopes) {
-		for (const local of scope.modifiedLocals()) {
-			colors.push([local, 'markup.underline'])
-		}
-	}
-
-	return colors
-}
-
-function colorTypescript(x: Parser.SyntaxNode, editor: vscode.TextEditor) {
-	const colors: [Parser.SyntaxNode, string][] = []
-	function scan(x: Parser.SyntaxNode) {
-		if (!isVisible(x, editor)) return
-		if (x.type == 'identifier' && x.parent != null && x.parent.type == 'function') {
-			colors.push([x, 'entity.name.function'])
-		} else if (x.type == 'type_identifier' || x.type == 'predefined_type') {
-			colors.push([x, 'entity.name.type'])
-		} else if (x.type == 'property_identifier') {
-			colors.push([x, 'variable'])
-		} else if (x.type == 'method_definition' && ['get', 'set'].includes(x.firstChild!.text)) {
-			colors.push([x.firstChild!, 'keyword'])
-		}
-		for (const child of x.children) {
-			scan(child)
-		}
-	}
-	scan(x)
-
-	return colors
-}
-
-function colorRuby(x: Parser.SyntaxNode, editor: vscode.TextEditor) {
-	const colors: [Parser.SyntaxNode, string][] = []
-	const control = ['while', 'until', 'if', 'unless', 'for', 'begin', 'elsif', 'else', 'ensure', 'when', 'case', 'do_block']
-	const variables = ['instance_variable', 'class_variable', 'global_variable']
-	function scan(x: Parser.SyntaxNode) {
-		if (!isVisible(x, editor)) return
-		if (x.type == 'method') {
-			colors.push([x.children[1]!, 'entity.name.function'])
-		} else if (x.type == 'singleton_method') {
-			colors.push([x.children[3], 'entity.name.function'])
-		} else if (variables.includes(x.type)) {
-			colors.push([x, 'variable'])
-		} else if (x.type == 'call' && x.lastChild!.type == 'identifier') {
-			colors.push([x.lastChild!, 'entity.name.function'])
-		} else if (x.type == 'method_call' && x.firstChild!.type == 'identifier') {
-			colors.push([x.firstChild!, 'entity.name.function'])
-		} else if (x.type == 'end') {
-			if (control.includes(x.parent!.type)) {
-				colors.push([x, 'keyword.control'])
-			} else {
-				colors.push([x, 'keyword'])
-			}
-		} else if (x.type == 'constant') {
-			colors.push([x, 'entity.name.type'])
-		} else if (x.type == 'symbol') {
-			colors.push([x, 'constant.numeric'])
-		}
-		for (const child of x.children) {
-			scan(child)
-		}
-	}
-	scan(x)
-
-	return colors
-}
-
-function colorRust(x: Parser.SyntaxNode, editor: vscode.TextEditor) {
-	const colors: [Parser.SyntaxNode, string][] = []
-	function looksLikeType(id: string) {
-		if (id.length == 0) return false
-		if (id[0] != id[0].toUpperCase()) return false
-		for (const c of id) {
-			if (c.toLowerCase() == c) return true
-		}
-		return false
-	}
-	function scanUse(x: Parser.SyntaxNode) {
-		if (x.type == 'identifier' && looksLikeType(x.text)) {
-			colors.push([x, 'entity.name.type'])
-		}
-		for (const child of x.children) {
-			scanUse(child)
-		}
-	}
-	function scan(x: Parser.SyntaxNode) {
-		if (!isVisible(x, editor)) return
-		if (x.type == 'identifier' && x.parent != null && x.parent.type == 'function_item' && x.parent.parent != null && x.parent.parent.type == 'declaration_list') {
-			colors.push([x, 'variable'])
-		} else if (x.type == 'identifier' && x.parent != null && x.parent.type == 'function_item') {
-			colors.push([x, 'entity.name.function'])
-		} else if (x.type == 'identifier' && x.parent != null && x.parent.type == 'scoped_identifier' && x.parent.parent != null && x.parent.parent.type == 'function_declarator') {
-			colors.push([x, 'entity.name.function'])
-		} else if (x.type == 'use_declaration') {
-			scanUse(x)
-			return
-		} else if (x.type == 'type_identifier' || x.type == 'primitive_type') {
-			colors.push([x, 'entity.name.type'])
-		} else if (x.type == 'field_identifier') {
-			colors.push([x, 'variable'])
-		}
-		for (const child of x.children) {
-			scan(child)
-		}
-	}
-	scan(x)
-
-	return colors
-}
-
-function colorCpp(x: Parser.SyntaxNode, editor: vscode.TextEditor) {
-	const colors: [Parser.SyntaxNode, string][] = []
-	function scan(x: Parser.SyntaxNode) {
-		if (!isVisible(x, editor)) return
-		if (x.type == 'identifier' && x.parent != null && x.parent.type == 'function_declarator') {
-			colors.push([x, 'entity.name.function'])
-		} else if (x.type == 'identifier' && x.parent != null && x.parent.type == 'scoped_identifier' && x.parent.parent != null && x.parent.parent.type == 'function_declarator') {
-			colors.push([x, 'entity.name.function'])
-		} else if (x.type == 'type_identifier') {
-			colors.push([x, 'entity.name.type'])
-		} else if (x.type == 'field_identifier') {
-			colors.push([x, 'variable'])
-		}
-		for (const child of x.children) {
-			scan(child)
-		}
-	}
-	scan(x)
-
-	return colors
-}
-
-function isVisible(x: Parser.SyntaxNode, editor: vscode.TextEditor) {
-	for (const visible of editor.visibleRanges) {
-		const overlap = x.startPosition.row <= visible.end.line+1 && visible.start.line-1 <= x.endPosition.row
-		if (overlap) return true
-	}
-	return false
+// Be sure to declare the language in package.json and include a minimalist grammar.
+const languages: {[id: string]: {module: string, color: colors.ColorFunction, parser?: Parser}} = {
+	'go': {module: 'tree-sitter-go', color: colors.colorGo},
+	'cpp': {module: 'tree-sitter-cpp', color: colors.colorCpp},
+	'rust': {module: 'tree-sitter-rust', color: colors.colorRust},
+	'ruby': {module: 'tree-sitter-ruby', color: colors.colorRuby},
+	'typescript': {module: 'tree-sitter-typescript', color: colors.colorTypescript},
+	'javascript': {module: 'tree-sitter-javascript', color: colors.colorTypescript},
 }
 
 // Create decoration types from scopes lazily
@@ -319,7 +22,7 @@ function decoration(scope: string): vscode.TextEditorDecorationType|undefined {
 		return decorationCache.get(scope)
 	}
 	// If `scope` is defined in the current theme, create a decoration for it
-	const textmate = colors.find(scope)
+	const textmate = scopes.find(scope)
 	if (textmate) {
 		const decoration = createDecorationFromTextmate(textmate)
 		decorationCache.set(scope, decoration)
@@ -328,7 +31,7 @@ function decoration(scope: string): vscode.TextEditorDecorationType|undefined {
 	// Otherwise, give up, there is no color available for this scope
 	return undefined
 }
-function createDecorationFromTextmate(themeStyle: colors.TextMateRuleSettings): vscode.TextEditorDecorationType {
+function createDecorationFromTextmate(themeStyle: scopes.TextMateRuleSettings): vscode.TextEditorDecorationType {
 	let options: vscode.DecorationRenderOptions = {}
 	options.rangeBehavior = vscode.DecorationRangeBehavior.OpenOpen
 	if (themeStyle.foreground) {
@@ -358,19 +61,9 @@ function createDecorationFromTextmate(themeStyle: colors.TextMateRuleSettings): 
 	return vscode.window.createTextEditorDecorationType(options)
 }
 
-// Be sure to declare the language in package.json and include a minimalist grammar.
-const languages: {[id: string]: {module: string, color: ColorFunction, parser?: Parser}} = {
-	'go': {module: 'tree-sitter-go', color: colorGo},
-	'cpp': {module: 'tree-sitter-cpp', color: colorCpp},
-	'rust': {module: 'tree-sitter-rust', color: colorRust},
-	'ruby': {module: 'tree-sitter-ruby', color: colorRuby},
-	'typescript': {module: 'tree-sitter-typescript', color: colorTypescript},
-	'javascript': {module: 'tree-sitter-javascript', color: colorTypescript},
-}
-
 // Load styles from the current active theme
 async function loadStyles() {
-	await colors.load()
+	await scopes.load()
 	// Clear old styles
 	for (const style of decorationCache.values()) {
 		style.dispose()
@@ -446,9 +139,9 @@ export async function activate(context: vscode.ExtensionContext) {
 		if (t == null) return
 		const language = languages[editor.document.languageId]
 		if (language == null) return
-		const colors = language.color(t.rootNode, editor)
+		const scopes = language.color(t.rootNode, visibleLines(editor))
 		const nodes = new Map<string, Parser.SyntaxNode[]>()
-		for (const [x, scope] of colors) {
+		for (const [x, scope] of scopes) {
 			if (!nodes.has(scope)) nodes.set(scope, [])
 			nodes.get(scope)!.push(x)
 		}
@@ -495,6 +188,14 @@ export async function activate(context: vscode.ExtensionContext) {
 		colorAllOpen()
 	}
 	activateLazily()
+}
+
+function visibleLines(editor: vscode.TextEditor) {
+	return editor.visibleRanges.map(range => {
+		const start = range.start.line
+		const end = range.end.line
+		return {start, end}
+	})
 }
 
 function range(x: Parser.SyntaxNode): vscode.Range {
