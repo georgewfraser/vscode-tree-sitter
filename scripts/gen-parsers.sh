7a5aa956833d06cca01b5257cf023b663e69dc4f
George Fraser <george@fivetran.com>
2020-02-23 17:29:02 +0000

    Compile using emcc

--- scripts/gen-parsers.sh
+++ scripts/gen-parsers.sh
@@ -5,13 +5,29 @@
 
 set -e
 
+function compile {
+parser_c=`find $2 -name parser.c`
+src_dir=`dirname $parser_c`
+emcc \
+    -o $1.wasm \
+    -Os \
+    -s WASM=1 \
+    -s SIDE_MODULE=1 \
+    -s TOTAL_MEMORY=65536000 \
+    -s NODEJS_CATCH_EXIT=0 \
+    -s ASSERTIONS=1 \
+    -s 'EXPORTED_FUNCTIONS=["_$1"]' \
+    -fno-exceptions \
+    -I $src_dir $parser_c
+}
+
 # Build parsers
-./node_modules/.bin/tree-sitter build-wasm ./node_modules/tree-sitter-go
-./node_modules/.bin/tree-sitter build-wasm ./node_modules/tree-sitter-cpp
-./node_modules/.bin/tree-sitter build-wasm ./node_modules/tree-sitter-ruby
-./node_modules/.bin/tree-sitter build-wasm ./node_modules/tree-sitter-rust
-./node_modules/.bin/tree-sitter build-wasm ./node_modules/tree-sitter-typescript/typescript
-./node_modules/.bin/tree-sitter build-wasm ./node_modules/tree-sitter-javascript
-./node_modules/.bin/tree-sitter build-wasm ./node_modules/tree-sitter-verilog
+compile tree-sitter-go node_modules/tree-sitter-go
+compile tree-sitter-cpp node_modules/tree-sitter-cpp
+compile tree-sitter-ruby node_modules/tree-sitter-ruby
+compile tree-sitter-rust node_modules/tree-sitter-rust
+compile tree-sitter-typescript node_modules/tree-sitter-typescript/typescript
+compile tree-sitter-javascript node_modules/tree-sitter-javascript
+compile tree-sitter-verilog node_modules/tree-sitter-verilog
 
 mv *.wasm parsers
